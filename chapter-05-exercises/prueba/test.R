5ceccdd45a8f04a9c47aa6ffec0ed26f88931460
Alejandro Garcia <a.gcastellanos@alumnos.upm.es>
2020-07-02 14:21:25 +0200

    Realizado capitulo 5

---
+++ chapter-05-exercises/prueba/test.R
@@ -0,0 +1,16 @@
+1+1
+# Guardar el valor en una variable
+minutes_in_a_year <- 365 * 24 * 60
+
+print (minutes_in_a_year)
+
+famous_writer <- "Octavia Butler"
+
+a <- 3
+b <- 4
+
+a > 2 & b < 5
+(a + b != 7) | a*b>0
+
+complex_variable <- 3 + 2i
+print(complex_variable**2)
